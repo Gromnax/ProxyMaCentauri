eec6b972a7581c906408144d1c21a7157aa2479b
Gromnax <hezon.morgan@orange.fr>
2026-05-21 14:15:38 +0200

    Corrected connections?

--- GraphController/graph_edit.gd
+++ GraphController/graph_edit.gd
@@ -1,6 +1,12 @@
 extends GraphEdit
 class_name GraphControlEdit
 
+var move_scene = preload("res://GraphController/GraphNodes/MoveNode.tscn")
+var pulsar_scene = preload("res://GraphController/GraphNodes/PulsarNode.tscn")
+var print_scene = preload("res://GraphController/GraphNodes/Printer.tscn")
+
+var child_number: int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -39,7 +45,7 @@ func on_output_emitted(source: Node, port: int, value:String) -> void:
 	var retrieved_connections = get_output_connections_from_node(source, port)
 	var child : GraphControlNode
 	for connection in retrieved_connections:
-		child = find_child(connection.node)
+		child = find_child(connection.node, false, false)
 		child.signal_received(connection.port, value)
 
 func get_output_connections_from_node(node:Node, port: int) -> Array:
@@ -52,3 +58,32 @@ func get_output_connections_from_node(node:Node, port: int) -> Array:
 			dict["port"] = connection["to_port"]
 			result.push_back(dict)
 	return result
+
+
+func _on_move_button_pressed() -> void:
+	var new_node : MoveNode = move_scene.instantiate()
+	new_node.set_name("GraphControlNode"+str(child_number))
+	child_number = child_number +1
+	add_child(new_node)
+
+
+func _on_pulsar_button_pressed() -> void:
+	var new_node : PulsarNode = pulsar_scene.instantiate()
+	add_child(new_node)
+	var new_name : StringName = StringName("GraphControlNode"+str(child_number))
+	new_node.set_name(new_name)
+	child_number = child_number +1
+	new_node.owner = self
+	print(new_node)
+	print(find_children("*"))
+
+
+func _on_print_button_pressed() -> void:
+	var new_node : Printer = print_scene.instantiate()
+	new_node.set_name("GraphControlNode"+str(child_number))
+	child_number = child_number +1
+	add_child(new_node)
+
+
+func _on_counter_button_pressed() -> void:
+	print("Pas encore prêt")
