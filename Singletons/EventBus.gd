10e2b7fc007b2c5456d64eeb9a00e638996956e5
Titancki <ouf.guillaume@gmail.com>
2026-05-21 14:20:36 +0200

    add unused id func

--- Singletons/EventBus.gd
+++ Singletons/EventBus.gd
@@ -3,6 +3,9 @@ extends Node
 #Ici on met en place les signaux que le robot devra écouter ou envoyer
 #Passer par un event bus permet de réduire le couplage du code et donc de le rendre plus flexible
 
+## Id de requête pour retour
+var request_id : int = 0
+
 @warning_ignore_start("unused_signal")
 # Actions emitted by graph
 signal move_right(id:int)
@@ -19,3 +22,7 @@ signal check_obj_right_response(id:int, response:bool)
 signal check_obj_left_response(id:int, response:bool)
 signal check_obj_under_response(id:int, response:bool)
 signal check_just_fell_response(id:int, response:bool)
+
+func get_unused_id() -> int:
+	request_id = request_id+1
+	return request_id-1
