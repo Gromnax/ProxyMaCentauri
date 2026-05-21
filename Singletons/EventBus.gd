cf394020550c8c7d48ff88469ff5a5508dbd41ae
Titancki <ouf.guillaume@gmail.com>
2026-05-21 14:08:52 +0200

    add id for signals

--- Singletons/EventBus.gd
+++ Singletons/EventBus.gd
@@ -4,21 +4,18 @@ extends Node
 #Passer par un event bus permet de réduire le couplage du code et donc de le rendre plus flexible
 
 @warning_ignore_start("unused_signal")
-#old
-signal pseudo_code_changed(pseudo_code : Array)
-
 # Actions emitted by graph
-signal move_right
-signal move_left
-signal jump_right
-signal jump_left
-signal check_obj_right
-signal check_obj_left
-signal check_obj_under
-signal check_just_fell
+signal move_right(id:int)
+signal move_left(id:int)
+signal jump_right(id:int)
+signal jump_left(id:int)
+signal check_obj_right(id:int)
+signal check_obj_left(id:int)
+signal check_obj_under(id:int)
+signal check_just_fell(id:int)
 # Triggers emitted by character
-signal check_is_arrived(response:bool)
-signal check_obj_right_response(response:bool)
-signal check_obj_left_response(response:bool)
-signal check_obj_under_response(response:bool)
-signal check_just_fell_response(response:bool)
+signal check_is_arrived(id:int, response:bool)
+signal check_obj_right_response(id:int, response:bool)
+signal check_obj_left_response(id:int, response:bool)
+signal check_obj_under_response(id:int, response:bool)
+signal check_just_fell_response(id:int, response:bool)
