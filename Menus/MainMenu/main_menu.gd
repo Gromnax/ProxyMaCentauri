00200ac07603f2057ec3ad8c33e3eb4efe2633ec
Titancki <ouf.guillaume@gmail.com>
2026-05-22 11:33:25 +0200

    added esthetics and cinematic

--- Menus/MainMenu/main_menu.gd
+++ Menus/MainMenu/main_menu.gd
@@ -4,6 +4,11 @@ extends Control
 func _ready() -> void:
 	PreloadBus.references["bgm_player"].set_stream(PreloadBus.bgm["title"])
 	PreloadBus.references["bgm_player"].play()
+	$LabelTitle.modulate = Color(1,1,1,0)
+	$VBoxMenu.modulate = Color(1,1,1,0)
+	var tween = get_tree().create_tween().set_parallel(true)
+	tween.tween_property($LabelTitle, "modulate", Color(1,1,1,1), 0.5)
+	tween.tween_property($VBoxMenu, "modulate", Color(1,1,1,1), 0.5)
 
 func _on_button_options_pressed() -> void:
 	PreloadBus.change_level("option_menu")
--- Singletons/AudioBus.gd
+++ Singletons/AudioBus.gd
@@ -1,9 +1,17 @@
 extends Node
 
 func update_bgm_volume(volume: float) -> void:
-	for audioplayer in get_tree().get_nodes_in_group("bgm"):
-		audioplayer.volume_db = linear_to_db(volume)
+	var bus_index = AudioServer.get_bus_index("BGM")
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(volume))
 		
 func update_sfx_volume(volume: float) -> void:
-	for audioplayer in get_tree().get_nodes_in_group("sfx"):
-		audioplayer.volume_db = linear_to_db(volume)
+	var bus_index = AudioServer.get_bus_index("SFX")
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(volume))
+
+func play_sfx(sfx):
+	var audio_player = AudioStreamPlayer.new()
+	audio_player.set_stream(sfx)
+	audio_player.bus = "SFX" # Optional: assign to a specific audio bus
+	add_child(audio_player)
+	audio_player.play()
+	audio_player.finished.connect(audio_player.queue_free)
